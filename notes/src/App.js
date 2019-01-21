a0bdb1ba4082377cac0d7924847407143685e97c
Milos Kallai <miloskallai@Miloss-Mac-mini.local>
2019-01-21 11:49:40 +0100

    initial setup

---
+++ notes/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ShowNotes from './ShowNotes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <ShowNotes />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ notes/src/ShowNotes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShowNotes = () => {
+  return <div>THIS IS FROM THE SHOW NOTES</div>;
+};
+
+export default ShowNotes;
