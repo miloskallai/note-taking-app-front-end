0a7ff2066ee922aeb47746f34e3eba678ba92081
Milos Kallai <miloskallai@Miloss-Mac-mini.local>
2019-01-21 23:50:38 +0100

    (feat)react router links added

--- notes/src/Components/NavBar.js
+++ notes/src/Components/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div className='nav-bar'>
       <ul>
         <li className='logo'>notes</li>
-        <li>My Notes</li>
-        <li>New Note</li>
+        <Link to='/'>
+          <li>My Notes</li>
+        </Link>
+        <Link to='/new'>
+          <li>New Note</li>
+        </Link>
         <li>Shared Notes</li>
       </ul>
     </div>
